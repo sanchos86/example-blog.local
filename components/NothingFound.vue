c54385cc58107246844d6a3a75b089d9566a7c57
alexander.aladin <alexander.aladin@smartseeds.ru>
2021-02-09 19:34:41 +0300

    add NothingFound.vue

---
+++ components/NothingFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nothing-found">
+    <div class="nothing-found__icon-wrap mb-4">
+      <i class="nothing-found__icon fas fa-search fa-4x" />
+    </div>
+    <ui-heading
+      title="Записей не найдено"
+      :level="4"
+      :bordered="false"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .nothing-found {
+    text-align: center;
+  }
+</style>
