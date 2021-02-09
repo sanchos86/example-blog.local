a178da6af910bfc36837194848a3abab6146bf38
alexander.aladin <alexander.aladin@smartseeds.ru>
2021-02-09 19:30:39 +0300

    add Footer.vue

---
+++ components/layout/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          &copy; {{ new Date().getFullYear() }} web-artisan.ru
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  .footer {
+    padding: map-get($gaps, 4) 0;
+    color: $color-white;
+    background-color: $color-accent;
+    text-align: center;
+  }
+</style>
