a0e2132fc2ce1fe842ef74ff47ee8146aa2c4aec
alexander.aladin <alexander.aladin@smartseeds.ru>
2021-02-11 19:40:53 +0300

    update default layout and add layout components

---
+++ components/layout/Aside.vue
@@ -0,0 +1,12 @@
+<template>
+  <aside class="aside"></aside>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,41 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="root">
+    <Header />
+    <main class="main py-4">
+      <div class="container">
+        <div class="row">
+          <nuxt />
+          <Aside />
+        </div>
+      </div>
+    </main>
+    <Footer />
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  import Aside from '~/components/layout/Aside.vue';
+  import Header from '~/components/layout/Header.vue';
+  import Footer from '~/components/layout/Footer.vue';
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  export default {
+    components: {
+      Aside,
+      Header,
+      Footer
+    }
+  };
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<style lang="scss" scoped>
+  .root {
+    height: 100%;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .main {
+    flex-grow: 1;
+  }
 </style>
