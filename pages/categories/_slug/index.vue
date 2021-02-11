60f3a8d19dbd2f17ecb7e0e3cd90d37db1d85ad7
alexander.aladin <alexander.aladin@smartseeds.ru>
2021-02-11 19:27:19 +0300

    add pages

---
+++ pages/categories/_slug/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Categories
+  </div>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/tags/_slug/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Tags
+  </div>
+</template>
+
+<script>
+  export default {
+  };
+</script>
+
+<style scoped>
+
+</style>
